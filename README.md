9e466dba9d789998663a2e9e21ba3be9c873c192
elnukakujo <jagernoe1s3@hotmail.com>
2024-11-24 14:16:21 -0500

    Refactored more

--- README.md
+++ README.md
@@ -6,10 +6,8 @@ Website showing users and champion data of League of Legends
 Define global flags in Context.js and import them in other components (not constant functions).
 
 ## Ideas for next stuff
-- [x] Update the mastery grades to match the after match icons in client
 - [ ] Filter matchhistory by queuetype and gamemode
 - [ ] Make dark mode and day mode pages using css var
-- [x] Clickable links for players in matches
 - [ ] Add a open/close menu to show more elements of games (check op.gg for ref)
 - [ ] Add tags with First Blood, First Tower etc...
 - [ ] Give more match infos on the same view, feeling like infos is to concised right now, (Maybe show less infos for matches when match menu closed)
---
+++ frontend/summonerinsight-app/src/reusable/WinrateCircle.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+import "../assets/css/reusable/WinrateCircle.css";
+
+export default function WinrateCircle({wins, totalGames, losses, playerMeanKda}) {
+    return (
+        <div className="win-loss-ratio">
+            <svg className="winrate-circle" width="110" height="110" viewBox="0 0 36 36">
+                <circle className="bg-circle" cx="18" cy="18" r="16"></circle>
+                <circle className="percent-circle" cx="18" cy="18" r="16"
+                        style={{"--percentage": `${wins/totalGames}`}}></circle>
+                <text x="18" y="20.35" textAnchor="middle" fontSize="8" fill="#E0E0E0">{(wins/totalGames*100).toFixed(0)}%</text>
+            </svg>
+            <div className="tooltip">
+                <p>{totalGames}G {wins}W {losses}L</p>
+                <p>Winrate: {((wins / totalGames) * 100).toFixed(0)}%</p>
+                <p>Average {playerMeanKda.toFixed(2)} KDA</p>
+            </div>
+        </div>
+    );
+}
