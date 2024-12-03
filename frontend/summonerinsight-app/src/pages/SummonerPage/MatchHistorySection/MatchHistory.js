3c5e6d38f4b92c967db5522bfde3e337dd9ed079
elnukakujo <jagernoe1s3@hotmail.com>
2024-12-02 22:02:32 -0500

    Refactored matchHistory for more compact display of history

--- frontend/summonerinsight-app/src/pages/SummonerPage/MatchHistorySection/MatchHistory.js
+++ frontend/summonerinsight-app/src/pages/SummonerPage/MatchHistorySection/MatchHistory.js
@@ -6,22 +6,19 @@ import '../../../assets/css/pages/SummonerPage/MatchHistorySection/MatchHistory.
 import LoadButton from "../../../components/LoadButton.js";
 import GameStatus from "./GameStatus.js";
 import PlayerInfos from "./PlayerInfos.js";
-import Team from "./Team.js";
 
 import {fetchAPIData} from "../../../api.js";
 import {api_url} from "../../../constants.js";
 
 function Match({match}){
     const { gameName } = useParams();
+    const [ isOpen, setIsOpen ] = useState(false);
+
     const playerStats = match.participants.find(participant => participant.gameName.toLowerCase().replace(/\s/g, '') === gameName);
     return(
-        <div className={`match ${playerStats.win ? 'win' : 'loss'}`}>
+        <div className={`match ${playerStats.win ? 'win' : 'loss'} ${isOpen?'extended':''}`} onClick={()=>setIsOpen(!isOpen)}>
             <GameStatus playerStats={playerStats} match={match}/>
             <PlayerInfos playerStats={playerStats} match={match}/>
-            <div className="team-composition section">
-                <Team match={match} id={100} gameName={gameName}/>
-                <Team match={match} id={200} gameName={gameName}/>
-            </div>
         </div>
     );
 }
